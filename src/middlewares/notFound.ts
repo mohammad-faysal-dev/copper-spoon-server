4e4a2fc0b405363c81a5c6ed271944591a4db2a5
Faysal2123 <foysal2005n1@gmail.com>
2026-09-15 22:09:09 +0600

    feat: add not found middleware

---
+++ src/middlewares/notFound.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export function notFound(req: Request, res: Response) {
+  res.status(400).json({
+    message: "Route not found",
+    path: req.originalUrl,
+  });
+}
