553e9258015fd0e983fd6c0cb9f1483d1b861993
Mohammad Faysal <162318772+mohammad-faysal-dev@users.noreply.github.com>
2026-09-18 12:45:15 +0600

    feat: add delete category route

--- src/modules/category/category.route.ts
+++ src/modules/category/category.route.ts
@@ -7,6 +7,7 @@ router.get("/", CategoryController.getAllCategories)
 router.post("/", CategoryController.createCategory)
 router.get("/:categoryId", CategoryController.getCategoryById)
 router.patch("/:categoryId", CategoryController.updateCategory)
+router.delete("/:categoryId", CategoryController.deleteCategory)
 
 
 export const categoryRoute = router 
