8f17be73ef4260e5b13d1c75eb341bdf5a78cec5
Mohammad Faysal <162318772+mohammad-faysal-dev@users.noreply.github.com>
2026-09-18 23:34:54 +0600

    feat: add provider authorization to get all meals route

--- src/modules/meal/meal.route.ts
+++ src/modules/meal/meal.route.ts
@@ -4,6 +4,7 @@ import auth, { UserRole } from "../../middlewares/auth"
 
 const router = express.Router()
 
+router.get("/", auth(UserRole.PROVIDER), MealController.getAllMeals)
 router.post("/", auth(UserRole.PROVIDER), MealController.createMeal)
 
 export const melaRoute = router
