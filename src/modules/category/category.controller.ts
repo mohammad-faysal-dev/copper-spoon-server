600911afa16a881b96ee329371ad1d362f62f04d
Mohammad Faysal <162318772+mohammad-faysal-dev@users.noreply.github.com>
2026-09-18 12:03:43 +0600

    feat: add get all categories controller

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { CategoryService } from "./category.service";
+import { success } from "better-auth";
 
 const createCategory = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -14,6 +15,20 @@ const createCategory = async (req: Request, res: Response, next: NextFunction) =
     }
 }
 
+const getAllCategories = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const catagorise = await CategoryService.getAllCategories()
+        res.status(200).json({
+            success: true,
+            data: catagorise
+        })
+    }
+    catch (e) {
+        next(e)
+    }
+}
+
 export const CategoryController = {
-    createCategory
+    createCategory,
+    getAllCategories
 }
