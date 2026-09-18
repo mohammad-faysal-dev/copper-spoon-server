4d927eed763a69a0b5724109f41b788005544b98
Mohammad Faysal <162318772+mohammad-faysal-dev@users.noreply.github.com>
2026-09-18 11:11:06 +0600

    feat: add meal creation service with provider validation

--- src/modules/meal/meal.service.ts
+++ src/modules/meal/meal.service.ts
@@ -1,1 +1,23 @@
-const createMeal = async (userId: string,)
+import { prisma } from "../../lib/prisma";
+import { CreateMealPayload } from "./meal.type";
+
+const createMeal = async (userId: string, data: CreateMealPayload) => {
+    const provider = await prisma.providerProfile.findUnique({
+        where: {
+            userId
+        }
+    })
+    if (!provider) {
+        throw new Error("Provider profile not found");
+    }
+    const result = await prisma.meal.create({
+        data: {
+            providerId: provider.id,
+            ...data
+        }
+    })
+    return result
+}
+export const MealService = {
+    createMeal
+}
