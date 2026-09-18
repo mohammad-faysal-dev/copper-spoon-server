122fd4f79ab8cc4ed5068cd75b8d79d5c7241abe
Mohammad Faysal <162318772+mohammad-faysal-dev@users.noreply.github.com>
2026-09-18 11:37:17 +0600

    feat: add create category controller

---
+++ src/modules/category/category.controller.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+
+const createCategory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const category = await CategoryService.createCategory(req.body)
+        res.status(200).json({
+            success: true,
+            data: category
+        })
+    }
+    catch (e) {
+        next(e)
+    }
+}
+
+export const CategoryController = {
+    createCategory
+}
