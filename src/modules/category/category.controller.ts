830bf39bcc7335605cbb288d67c60ff4191d49d0
Mohammad Faysal <162318772+mohammad-faysal-dev@users.noreply.github.com>
2026-09-18 12:28:26 +0600

    feat: add update category controller

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -31,7 +31,7 @@ const getAllCategories = async (req: Request, res: Response, next: NextFunction)
 const getCategoryById = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { categoryId } = req.params
-        const category = await CategoryService.getCategoryById(categoryId)
+        const category = await CategoryService.getCategoryById(categoryId as string)
         res.status(200).json({
             success: true,
             data: category
@@ -42,8 +42,24 @@ const getCategoryById = async (req: Request, res: Response, next: NextFunction) 
     }
 }
 
+const updateCategory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { categoryId } = req.params
+        const category = await CategoryService.updateCategory(categoryId, req.body)
+        res.status(200).json({
+            success: true,
+            message: "Category updated successfully",
+            data: category,
+        })
+    }
+    catch (e) {
+        next(e)
+    }
+}
+
 export const CategoryController = {
     createCategory,
     getAllCategories,
-    getCategoryById
+    getCategoryById,
+    updateCategory
 }
