9ec89149b4ecb6f34b010aa63be4ce11ab9093b7
Mohammad Faysal <162318772+mohammad-faysal-dev@users.noreply.github.com>
2026-09-19 00:10:33 +0600

    feat: add delete meal controller

--- src/modules/meal/meal.controller.ts
+++ src/modules/meal/meal.controller.ts
@@ -33,8 +33,20 @@ const getMealById = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
+const deleteMeal = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { mealId } = req.body
+        const meal = await MealService.deleteMeal(mealId as string)
+        res.status(200).json(meal)
+    }
+    catch (e) {
+        next(e)
+    }
+}
+
 export const MealController = {
     createMeal,
     getAllMeals,
-    getMealById
+    getMealById,
+    deleteMeal
 }
