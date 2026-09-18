144c6601e2a48e46ccb947bb9c2151d57130e7d2
Mohammad Faysal <162318772+mohammad-faysal-dev@users.noreply.github.com>
2026-09-18 12:07:47 +0600

    feat: add get all categories route

--- src/modules/category/category.route.ts
+++ src/modules/category/category.route.ts
@@ -2,6 +2,7 @@ import { CategoryController } from "./category.controller"
 import express from "express"
 
 const router = express.Router()
+router.get("/", CategoryController.getAllCategories)
 router.post("/", CategoryController.createCategory)
 
 export const categoryRoute = router 
